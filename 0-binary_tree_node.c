9384fb2be338bcd94c32c82e401bee7b61285aab
Gich-M <gichmuriuki21@gmail.com>
2024-02-29 01:40:49 -0800

    Error fix

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -18,8 +18,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 		return (NULL);
 	}
 
-	new->parent = parent;
 	new->n = value;
+	new->parent = parent;
 	new->left = NULL;
 	new->right = NULL;
 
