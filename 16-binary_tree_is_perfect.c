98ba92398a162ac069a33f06f12768b743860296
Gich-M <gichmuriuki21@gmail.com>
2024-03-08 07:37:38 -0800

    perfect tree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -2,15 +2,45 @@
 
 /**
  * binary_tree_is_perfect - Checks if a binary tree is perfect.
- * @tree: Pointer to the root node of the tree to check.
+ * @tree: A pointer to the root node of the tree to check.
  *
- * Return: 0 if tree is NULL
-*/
+ * Return: If the tree is perfect, 1. Otherwise, 0.
+ */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	int lh, rh;
+
 	if (tree == NULL)
 		return (0);
 
-	return (binary_tree_is_perfect(tree->left) &&
-			binary_tree_is_perfect(tree->right));
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	lh = binary_tree_height(tree->left);
+	rh = binary_tree_height(tree->right);
+
+	if (lh == rh &&
+			binary_tree_is_perfect(tree->left) &&
+			binary_tree_is_perfect(tree->right))
+		return (1);
+	return (0);
 }
+
+/**
+ * binary_tree_height - Computes the height of a binary tree.
+ * @tree: A pointer to the root node of the tree.
+ *
+ * Return: The height of the tree.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t lh, rh;
+
+	if (tree == NULL)
+		return (0);
+
+	lh = binary_tree_height(tree->left);
+	rh = binary_tree_height(tree->right);
+	return ((lh > rh ? lh : rh) + 1);
+}
+
